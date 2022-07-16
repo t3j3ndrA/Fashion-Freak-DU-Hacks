5d35acb4b9acd21e2646599582e423cc615125ac
t3j3ndrA <tejendradhanani@gmail.com>
2022-07-17 00:58:05 +0530

    MongoDB database testings

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 app.use(cors());
 
+// Dotenv Configuration
+dotenv.config();
+
+// Middlewares
+// To use API in front-end
+
+// To use JSON data
+app.use(express.json());
+
+// DB Connection
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log(`DB Connection Successful`))
+	.catch((e) => console.log("err >> ", e));
+
 // Server Listening...
 const msg = `Service is running on port 5001`;
 app.listen(process.env.PORT || 5001, () => console.log(msg));
